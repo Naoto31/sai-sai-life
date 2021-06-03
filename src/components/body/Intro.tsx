6a91f255101e79313e39dfc95771de6c0abca532
Naoto31 <naoto0910sai@gmail.com>
2021-06-03 14:45:54 +0200

    created intro part

---
+++ src/components/body/Intro.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+interface IntroProps {}
+
+const Intro: React.FunctionComponent<IntroProps> = ({}) => {
+  return <div><p>Greetings from Munich</p></div>;
+};
+
+export default Intro;
