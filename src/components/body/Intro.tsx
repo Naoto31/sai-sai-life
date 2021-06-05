3008e376a23815ca69374c00aa87f4bb286255b4
Naoto31 <naoto0910sai@gmail.com>
2021-06-05 21:17:26 +0200

    added youtube video in intro

--- src/components/body/Intro.tsx
+++ src/components/body/Intro.tsx
@@ -3,7 +3,25 @@ import * as React from 'react';
 interface IntroProps {}
 
 const Intro: React.FunctionComponent<IntroProps> = ({}) => {
-  return <div><p>Greetings from Munich</p></div>;
+  return (
+    <div className='mt-12'>
+      <section id='profile' className="w-full">
+        <h2 className='text-center text-muted text-4xl text-gray-400'>
+          Greetings from Munich!
+        </h2>
+        <div>
+          <iframe
+            className="m-auto"
+            width='560'
+            height='315'
+            src='https://www.youtube.com/embed/zT_wR_f_AS0'
+            title='YouTube video player'
+            allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
+          ></iframe>
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default Intro;
