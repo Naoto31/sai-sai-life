8734d9a8f752b36afc0c9eb8d96eebc5ad5a0aa2
Zargham Ahmad Khan <Home@Administrators-MacBook-Pro.local>
2021-06-13 18:04:03 +0200

    added map

--- src/components/body/Worldmap.tsx
+++ src/components/body/Worldmap.tsx
@@ -1,47 +1,64 @@
 import { useEffect, useRef } from "react";
+import * as d3 from "d3";
+import * as topojson from "topojson-client";
+interface Worldmap {}
 
+const Worldmap: React.FC<Worldmap> = () => {
+  const graphRef = useRef<HTMLDivElement | null>(null);
+  useEffect(() => {
+    const getDataAndRenderChart = async () => {
+      const data = await fetch(
+        "https://piwodlaiwo.github.io/topojson//world-continents.json"
+      ).then((res) => res?.json());
+      const continents = topojson.feature(
+        data,
+        data.objects.continent
+      ).features;
 
-interface Worldmap {
+      if (!graphRef.current) return;
+      createChart(graphRef.current, continents);
+    };
 
-}
-
-const Worldmap:React.FC<Worldmap> = ()=>{
-    const graphRef = useRef<HTMLDivElement|null>(null)
-    useEffect(() => {
-        if(!graphRef.current) return;
-        createChart(graphRef.current)
-    }, [])
-    return <div className="w-full bg-gray-100 py-10" style={{height:"680px"}}>
-        <div className="w-2/3 bg-red-300 mx-auto h-full" ref={graphRef}>
-
-        </div>
+    getDataAndRenderChart();
+  }, []);
+  return (
+    <div className="w-full py-10 bg-white" style={{ height: "680px" }}>
+      <div className="w-2/3 h-full mx-auto " ref={graphRef}></div>
     </div>
-}
+  );
+};
 
 export default Worldmap;
 
-function createChart(container:HTMLDivElement){
-    // const data = {};
-    // const pathGenerator = d3.geoPath().projection(d3.geoMercator());
-    // const svg = d3
-    //   .select(container)
-    //   .append('svg')
-    //   .attr('width', '100%')
-    //   .attr('height', '100%')
-    //   .attr('style', 'background:blue')
+function createChart(container: HTMLDivElement, data: any) {
+  d3.select(".svgCont").remove();
+  const svg = d3
+    .select(container)
+    .append("svg")
+    .attr("width", "100%")
+    .attr("height", "100%")
+    .attr("class", "svgCont");
 
-    // const mapG = svg.append("g").attr("class","mapG")
+  const projection = d3.geoMercator().center([0, 60]).scale(130);
 
+  const pathGenerator = d3.geoPath().projection(projection);
 
+  const mapG = svg.append("g").attr("class", "mapG");
 
-    // const paths = mapG.selectAll('path').data(data.features);
-    // paths
-    //   .enter()
-    //   .append('path')
-    //   .attr("class","svg-country")
-    //   .attr('d', (d) => pathGenerator(d))
-    //   .attr('fill', (d) => "#ccc")
+  const paths = mapG
+    .selectAll("path")
+    .data(data)
+    .join("path")
+    .attr("class", "continent")
+    .attr("d", (d: any) => pathGenerator(d))
+    .attr("fill", (d) => "#ccc");
 
-
-   
+  paths.on("mouseenter", (d) => {
+    const path = d.target as SVGPathElement;
+    path.setAttribute("fill", "lightblue");
+  });
+  paths.on("mouseout", (d) => {
+    const path = d.target as SVGPathElement;
+    path.setAttribute("fill", "#ccc");
+  });
 }
