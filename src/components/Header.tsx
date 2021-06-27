9bc33ec26e7727520c8c25f58a550af545dc4164
Naoto31 <naoto0910sai@gmail.com>
2021-06-27 18:00:20 +0200

    added sns link

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,14 +10,43 @@ import { GrFacebookOption } from 'react-icons/gr';
 interface HeaderProps {}
 
 const Header: React.FunctionComponent<HeaderProps> = ({}) => {
+  const twitterUrl = 'https://twitter.com/Nao10sai';
+  const InstagramUrl = 'https://www.instagram.com/saisai.life/';
+  const FacebookUrl = 'https://www.facebook.com/saisai.life';
+  const YouTubeUrl = 'https://www.youtube.com/channel/UCmW8u1MGJ0ixIMXLahnPf3g';
+
+  const openSNS = (url: string) => {
+    window.open(url, '_blank');
+  };
+
   return (
     <div className='w-full bg-gray-800 h-12'>
       <div className='w-full  h-full max-w-6xl mx-auto flex justify-end'>
         <div className='flex mr-10 items-center'>
-          <AiOutlineTwitter className='w-6 h-6 text-gray-400 hover:text-gray-200 cursor-pointer mx-2' />
-          <AiOutlineInstagram className='w-6 h-6 text-gray-400 hover:text-gray-200 cursor-pointer mx-2' />
-          <GrFacebookOption className='w-6 h-6 text-gray-400 hover:text-gray-200 cursor-pointer mx-2' />
-          <AiFillYoutube className='w-6 h-6 text-gray-400 hover:text-gray-200 cursor-pointer mx-2' />
+          <AiOutlineTwitter
+            className='w-6 h-6 text-gray-400 hover:text-gray-200 cursor-pointer mx-2'
+            onClick={(e) => {
+              openSNS(twitterUrl);
+            }}
+          />
+          <AiOutlineInstagram
+            className='w-6 h-6 text-gray-400 hover:text-gray-200 cursor-pointer mx-2'
+            onClick={(e) => {
+              openSNS(InstagramUrl);
+            }}
+          />
+          <GrFacebookOption
+            className='w-6 h-6 text-gray-400 hover:text-gray-200 cursor-pointer mx-2'
+            onClick={(e) => {
+              openSNS(FacebookUrl);
+            }}
+          />
+          <AiFillYoutube
+            className='w-6 h-6 text-gray-400 hover:text-gray-200 cursor-pointer mx-2'
+            onClick={(e) => {
+              openSNS(YouTubeUrl);
+            }}
+          />
         </div>
         <div className='w-12 h-full bg-blue-400 flex justify-center items-center'>
           <AiOutlineSearch className='w-8 h-8 text-white fill-current' />
